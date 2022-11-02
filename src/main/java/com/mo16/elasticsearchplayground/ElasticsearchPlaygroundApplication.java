e89b81a06cb82593b7aae66f5c5f66f39345ca0d
mosup emad <mosup16@gmail.com>
2022-11-02 23:55:14 +0200

    initial commit

---
+++ src/main/java/com/mo16/elasticsearchplayground/ElasticsearchPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.mo16.elasticsearchplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mo16/elasticsearchplayground/ElasticsearchPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mo16.elasticsearchplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
