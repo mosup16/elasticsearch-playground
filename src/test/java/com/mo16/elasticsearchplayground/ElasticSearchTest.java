4e88cc503f17b907a97acb4e69047a42d780474f
mosup emad <mosup16@gmail.com>
2022-11-12 23:30:50 +0200

    send fake CPU usage read document to elasticsearch

--- src/test/java/com/mo16/elasticsearchplayground/ElasticSearchTest.java
+++ src/test/java/com/mo16/elasticsearchplayground/ElasticSearchTest.java
@@ -1,6 +1,7 @@
 package com.mo16.elasticsearchplayground;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.UUID;
 
 @SpringBootTest
@@ -29,5 +31,19 @@ public class ElasticSearchTest {
                     .id(id.toString()));
         }
         System.out.println("taken time = " + (System.currentTimeMillis() - start));
+    }
+
+    @Test
+    @Disabled
+    void sendFakeCPUUsageReadDocument() throws IOException {
+        record CpuUsage(UUID id, double usage, LocalDateTime readAt){}
+
+        IndexResponse response = elasticsearch.index(i -> {
+            CpuUsage usage = new CpuUsage(UUID.randomUUID(), new Random().nextDouble(100),
+                    LocalDateTime.now());
+            return i.index("fake-cpu-usage").document(usage).id(usage.id().toString());
+        });
+
+        System.out.println(response);
     }
 }
