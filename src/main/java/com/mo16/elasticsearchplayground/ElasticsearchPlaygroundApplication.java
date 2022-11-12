8085135489f20015ffdc480fd9ae9660292ba702
mosup emad <mosup16@gmail.com>
2022-11-12 19:23:42 +0200

    add java elasticsearch client configurations

--- src/main/java/com/mo16/elasticsearchplayground/ElasticsearchPlaygroundApplication.java
+++ src/main/java/com/mo16/elasticsearchplayground/ElasticsearchPlaygroundApplication.java
@@ -1,13 +1,47 @@
 package com.mo16.elasticsearchplayground;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ElasticsearchPlaygroundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticsearchPlaygroundApplication.class, args);
+    }
+
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient(
+            @Value("${application.elasticsearch.hostname}") String hostname,
+            @Value("${application.elasticsearch.port}") int port,
+            @Value("${application.elasticsearch.username}") String username,
+            @Value("${application.elasticsearch.password}") String password) {
+
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+
+        RestClient restClient = RestClient.builder(new HttpHost(hostname, port,"https"))
+                .setHttpClientConfigCallback(cb -> cb.setDefaultCredentialsProvider(credentialsProvider))
+                .build();
+
+        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
     }
 
 }
